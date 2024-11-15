ceae5e9f22593eca7bf9b0ee3dd4a4254a77c793
PhilippSchweizer <ph.m.schweizer@gmail.com>
2024-11-15 11:28:02 +0100

    prevent creation of duplicates

--- schemas/tag.ts
+++ schemas/tag.ts
@@ -1,5 +1,6 @@
 import {defineField, defineType} from 'sanity'
 import {TagIcon} from '@sanity/icons'
+import {Rule, StringRule, ValidationContext} from '@sanity/types'
 
 export default defineType({
   name: 'tag',
@@ -11,6 +12,39 @@ export default defineType({
       name: 'title',
       title: 'Titel',
       type: 'string',
+
+      validation: (rule: StringRule) =>
+        rule.required().custom(async (title: string | undefined, context: ValidationContext) => {
+          // Skip validation if updating an existing document
+          if (!context?.document || !title) {
+            return true
+          }
+
+          const client = context.getClient({apiVersion: '2024-03-25'})
+
+          // Get the base document ID (remove the draft suffix)
+          const baseID = context.document._id.replace(/^drafts\./, '')
+
+          const query = `(
+            *[
+              _type == "tag" && 
+              title == $title && 
+              !(_id in [$currentId, $draftId])
+            ]
+          )`
+
+          const duplicates = await client.fetch(query, {
+            title: title?.trim(),
+            currentId: baseID,
+            draftId: `drafts.${baseID}`,
+          })
+
+          if (duplicates.length > 0) {
+            return `Ein Schlagwort mit dem Titel "${title}" existiert bereits`
+          }
+
+          return true
+        }),
     }),
   ],
 })
